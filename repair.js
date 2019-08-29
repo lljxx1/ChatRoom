a103a50740196831456a5d57641e9c6e82582a2e
lljxx1 <lljxx1@gmail.com>
2019-08-29 13:30:28 +0800

    a

--- repair.js
+++ repair.js
@@ -5,12 +5,16 @@ var redisHost = '127.0.0.1';
 var redisCli = new Redis(redisPort, redisHost);
 
 (async () => {
-
     var msg = await redisCli.pipeline().lindex('room_wf5mfgmui7grb546_recentmsg', 1).exec();
     console.log(msg);
     var d = JSON.parse(msg[0][1]);
     if(d.from.name.indexOf('游客') > -1){
         console.log(d);
+        d.from.name = 'zhangdi';
+        d.from.role = 'vip';
+        d.from.avatarUrl = 'http://adbugsns.oss-cn-hangzhou.aliyuncs.com/headFile/201908/201908281817145d6654aa007a2.jpg';
+        d.from.uid = 20;
     }
-    
+
+    redisCli.pipeline().lset('room_wf5mfgmui7grb546_recentmsg', 1, JSON.stringify(d));
 })();;
