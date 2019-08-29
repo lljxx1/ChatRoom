a163abe018c10891741a280e3d07f1632196d6db
lljxx1 <lljxx1@gmail.com>
2019-08-29 13:23:40 +0800

    a

---
+++ repair.js
@@ -0,0 +1,12 @@
+
+var Redis = require("ioredis");
+var redisPort = 6375;
+var redisHost = '127.0.0.1';
+var redisCli = new Redis(redisPort, redisHost);
+
+(async () => {
+
+    var msg = redisCli.pipeline().lindex('room_wf5mfgmui7grb546_recentmsg', 1).exec();
+    console.log(msg);
+    
+})();;
