72ff0cfdf65b6cee7ba20c8ec512243cfc0a4b25
lljxx1 <lljxx1@gmail.com>
2019-08-29 13:33:37 +0800

    a

--- repair.js
+++ repair.js
@@ -5,7 +5,7 @@ var redisHost = '127.0.0.1';
 var redisCli = new Redis(redisPort, redisHost);
 
 (async () => {
-    var msg = await redisCli.pipeline().lindex('room_4j2e8xk1uimg1gir_recentmsg', 1).exec();
+    var msg = await redisCli.pipeline().lindex('room_4j2e8xk1uimg1gir_recentmsg', 2).exec();
     console.log(msg);
     var d = JSON.parse(msg[0][1]);
     if(d.from.name.indexOf('游客') > -1){
@@ -14,7 +14,7 @@ var redisCli = new Redis(redisPort, redisHost);
         d.from.role = 'vip';
         d.from.avatarUrl = 'http://adbugsns.oss-cn-hangzhou.aliyuncs.com/headFile/201908/201908281817145d6654aa007a2.jpg';
         d.from.uid = 20;
-        var res = await redisCli.pipeline().lset('room_4j2e8xk1uimg1gir_recentmsg', 1, JSON.stringify(d)).exec();
+        var res = await redisCli.pipeline().lset('room_4j2e8xk1uimg1gir_recentmsg', 2, JSON.stringify(d)).exec();
         console.log(res);
     }
 
