1e2027253ffb5d1ce1ba2b723906b25bba38eae9
lljxx1 <lljxx1@gmail.com>
2019-08-29 13:26:45 +0800

    a

--- repair.js
+++ repair.js
@@ -8,7 +8,7 @@ var redisCli = new Redis(redisPort, redisHost);
 
     var msg = await redisCli.pipeline().lindex('room_wf5mfgmui7grb546_recentmsg', 1).exec();
     console.log(msg);
-    var d = JSON.parse(msg);
+    var d = JSON.parse(msg[0][1]);
     if(d.from.name.indexOf('游客') > -1){
         console.log(d);
     }
